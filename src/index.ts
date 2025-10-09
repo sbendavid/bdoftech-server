9a00edc013456404960474e35453e9cc74133e81
sbendavid <samuelbendavid01@gmail.com>
2025-10-09 17:46:34 +0100

    chore: updated index

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
-// import serverless from "serverless-http";
 import CONFIG from "./config/config";
 import connectDB from "./config/db";
 import { errorMiddleware } from "./exceptions/errors";
@@ -30,8 +29,6 @@ app.get("/", (_req, res) => {
 app.use("/api/v1", routes);
 
 app.use(errorMiddleware);
-
-// serverless(app);
 
 app.listen(PORT, () => {
   console.log(`Server running locally at http://localhost:${PORT}`);
